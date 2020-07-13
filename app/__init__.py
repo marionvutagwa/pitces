36f21b32c9edbe1860760c2f5acf55c332168e15
marion vutagwa <marionvutagwa@yahoo.com>
2020-07-13 22:56:06 +0300

    add flask-bootsrap to enable styling

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from .config import DevConfig
+from flask_bootstrap import Bootstrap
 
 # Initializing application
-app = Flask(__name__)
+app = Flask(__name__,instance_relative_config = True)
 
 # Setting up configuration
 app.config.from_object(DevConfig)
+app.config.from_pyfile("config.py")
 
-from app import views
+# Initializing Flask Extensions
+bootstrap = Bootstrap(app)
+...

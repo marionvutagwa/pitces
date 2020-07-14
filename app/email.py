be96bba2f937e4d938eb971f7723c70542ef3e15
marion vutagwa <marionvutagwa@yahoo.com>
2020-07-15 01:24:27 +0300

    add manage.py file

--- app/email.py
+++ app/email.py
@@ -4,7 +4,7 @@ from . import mail
 
 
 def mail_message(subject,template,to,**kwargs):
-    sender_email = 'sumeyahassan34@gmail.com'
+    sender_email = 'marionhassan34@gmail.com'
 
     email = Message(subject, sender=sender_email, recipients=[to])
     email.body= render_template(template + ".txt",**kwargs)
---
+++ config.py
@@ -0,0 +1,22 @@
+import os
+
+class Config:
+    
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mia:newPass@localhost/pitch'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SENDER_EMAIL = 'marionhassan34@gmail.com'
+
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True    
+    @staticmethod
+    def init_app(app):
+        pass
